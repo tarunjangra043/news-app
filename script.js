b18341f28c436902e668c7cf25672aeb2e3c4850
Tarun Jangra <111531292+tarunjangra043@users.noreply.github.com>
2024-10-18 13:27:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,12 +14,12 @@ const logo = document.getElementById("logo");
 var newsDataArr = [];
 
 const API_KEY = `a04b4af0c9a6422fa84e33fe3bbc81f4`;
-const HEADLINES_NEWS = `https://newsapi.org/v2/top-headlines?country=in&apiKey=`;
-const GENERAL_NEWS = `https://newsapi.org/v2/top-headlines?country=in&category=general&apiKey=`;
-const BUSINESS_NEWS = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=`;
-const SPORTS_NEWS = `https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=`;
-const ENTERTAINMENT_NEWS = `https://newsapi.org/v2/top-headlines?country=in&category=entertainment&apiKey=`;
-const TECHNOLOGY_NEWS = `https://newsapi.org/v2/top-headlines?country=in&category=technology&apiKey=`;
+const HEADLINES_NEWS = `https://newsapi.org/v2/top-headlines?country=us&apiKey=`;
+const GENERAL_NEWS = `https://newsapi.org/v2/top-headlines?country=us&category=general&apiKey=`;
+const BUSINESS_NEWS = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=`;
+const SPORTS_NEWS = `https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=`;
+const ENTERTAINMENT_NEWS = `https://newsapi.org/v2/top-headlines?country=us&category=entertainment&apiKey=`;
+const TECHNOLOGY_NEWS = `https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=`;
 const SEARCH_NEWS = `https://newsapi.org/v2/everything?q=`;
 
 window.onload = () => {
